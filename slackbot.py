01d083f448dd95522bf005d46287f8b17e022df4
wizarddk95 <102998769+wizarddk95@users.noreply.github.com>
2025-03-04 14:05:00 +0900

    Create slackbot.py

---
+++ slackbot.py
@@ -0,0 +1,18 @@
+import json
+import requests
+
+def post_to_slack(message):
+    webhook_url = 'https://hooks.slack.com/services/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'
+    slack_data = json.dumps({'text':message})
+    response = requests.post(
+        webhook_url, data=slack_data,
+        headers={'Content-Type': 'application/json'}
+    )
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
+
+if __name__ == '__main__':
+    post_to_slack('hello')
